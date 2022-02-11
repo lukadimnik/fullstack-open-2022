fcb064a999a2ff1c1eba0dd7ad6570763be80765
Luka <luka.dimnik88@gmail.com>
2022-02-11 21:09:18 +0200

    PART-2 Exercises 2.4 2.5

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Total = ({ parts }) => {
-  console.log(parts);
   const total = parts.reduce((prev, cur) => prev + cur.exercises, 0);
   return <strong>Total of {total} exercises</strong>;
 };
