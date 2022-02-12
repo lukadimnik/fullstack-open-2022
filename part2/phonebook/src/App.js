7c0fa34eee422496de843ab5311a04ad56953843
Luka <luka.dimnik88@gmail.com>
2022-02-12 09:09:13 +0200

    PART-2 Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '72837-899-234' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
   const [newName, setNewName] = useState('');
   const [phoneNumber, setphoneNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -15,12 +19,16 @@ const App = () => {
     setphoneNumber(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
     const doesExist =
       persons.filter((person) => person.name === newName).length > 0;
     if (!doesExist) {
-      setPersons([...persons, { name: newName, phone: phoneNumber }]);
+      setPersons([...persons, { name: newName, number: phoneNumber }]);
       setNewName('');
       setphoneNumber('');
     } else {
@@ -29,17 +37,31 @@ const App = () => {
   };
 
   const renderPersons = () => {
-    return persons.map((person, i) => (
-      <tr key={i}>
-        <td>{person.name}</td>
-        <td>{person.phone}</td>
-      </tr>
-    ));
+    return persons
+      .filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+      .map((person, i) => (
+        <tr key={i}>
+          <td>{person.name}</td>
+          <td>{person.number}</td>
+        </tr>
+      ));
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <label>
+        Filter shown with{' '}
+        <input
+          type='text'
+          name='filter'
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </label>
+      <h2>Add a New Entry</h2>
       <form onSubmit={handleFormSubmit}>
         <label>
           name:{' '}
@@ -65,7 +87,6 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
       <table>
         <tbody>{renderPersons()}</tbody>
       </table>
