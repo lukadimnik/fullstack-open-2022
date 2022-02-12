26d24f3e634256bb4fc5c7b4acf2945b865c93ec
Luka <luka.dimnik88@gmail.com>
2022-02-12 11:26:11 +0200

    PART-2 Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [phoneNumber, setphoneNumber] = useState('');
   const [filter, setFilter] = useState('');
@@ -38,6 +34,16 @@ const App = () => {
       window.alert(`Name: ${newName} already exists in the phonebook!`);
     }
   };
+
+  const fetchPersons = async () => {
+    const response = await axios('http://localhost:3001/persons');
+    setPersons(response.data);
+    console.log('response', response);
+  };
+
+  useEffect(() => {
+    fetchPersons();
+  }, []);
 
   return (
     <div>
