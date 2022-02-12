63cdf4dc02db1b4acb65e2e676c7a4804c75d18e
Luka <luka.dimnik88@gmail.com>
2022-02-12 08:59:14 +0200

    PART-2 Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '72837-899-234' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [phoneNumber, setphoneNumber] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    setphoneNumber(event.target.value);
   };
 
   const handleFormSubmit = (event) => {
@@ -13,22 +20,28 @@ const App = () => {
     const doesExist =
       persons.filter((person) => person.name === newName).length > 0;
     if (!doesExist) {
-      setPersons([...persons, { name: newName }]);
+      setPersons([...persons, { name: newName, phone: phoneNumber }]);
       setNewName('');
+      setphoneNumber('');
     } else {
       window.alert(`Name: ${newName} already exists in the phonebook!`);
     }
   };
 
   const renderPersons = () => {
-    return persons.map((person, i) => <li key={i}>{person.name}</li>);
+    return persons.map((person, i) => (
+      <tr key={i}>
+        <td>{person.name}</td>
+        <td>{person.phone}</td>
+      </tr>
+    ));
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleFormSubmit}>
-        <div>
+        <label>
           name:{' '}
           <input
             type='text'
@@ -36,14 +49,26 @@ const App = () => {
             value={newName}
             onChange={handleNameChange}
           />
-        </div>
+        </label>
+        <br />
+        <label>
+          number:{' '}
+          <input
+            type='text'
+            name='phoneNumber'
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+          />
+        </label>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>debug: {newName}</div>
-      {renderPersons()}
+      <table>
+        <tbody>{renderPersons()}</tbody>
+      </table>
     </div>
   );
 };
