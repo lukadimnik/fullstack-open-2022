0c4b6cdd6abda3f175652c643a73d68bc8232705
Luka <luka.dimnik88@gmail.com>
2022-03-15 08:57:08 +0200

    Part-4 Exercise 4.18

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -9,6 +9,7 @@ const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
+const loginRouter = require('./controllers/login');
 
 const mongoUrl = config.MONGODB_URI;
 logger.info('Connecting to the database...', config.MONGODB_URI);
@@ -24,6 +25,7 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 
