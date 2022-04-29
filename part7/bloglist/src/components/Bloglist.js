b4fc39747eb7b07cd7d204bab0e5d8a89e782f65
Luka <luka.dimnik88@gmail.com>
2022-04-29 18:40:42 +0300

    Part-7 Exercise 7.21

--- part7/bloglist/src/components/Bloglist.js
+++ part7/bloglist/src/components/Bloglist.js
@@ -1,22 +1,61 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import Blog from './Blog';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { increaseLike, removeBlog } from '../reducers/blogReducer';
+import { Button } from '@mui/material';
 
 const Bloglist = ({ blogs }) => {
+  const dispatch = useDispatch();
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Button</th>
-        </tr>
-      </thead>
-      <tbody>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} />
-        ))}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell align="right">Author</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {blogs.map((blog, i) => (
+            <TableRow
+              key={i}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+              </TableCell>
+              <TableCell align="right">{blog.author}</TableCell>
+              <TableCell align="right">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => dispatch(increaseLike(blog))}
+                >
+                  like
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => dispatch(removeBlog(blog.id))}
+                >
+                  delete
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
