1e739a27b72b080f4cc273df8b17fe3cce0cd73b
Luka <luka.dimnik88@gmail.com>
2022-05-18 09:24:15 +0300

    Part-8 Exercise 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,7 +1,9 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { ALL_BOOKS } from '../Queries';
 
 const Books = (props) => {
+  const [selectedGenre, setSelectedGenre] = useState(null);
   const result = useQuery(ALL_BOOKS);
   if (!props.show) {
     return null;
@@ -12,6 +14,48 @@ const Books = (props) => {
   }
 
   const books = result.data.allBooks;
+  const genresArr = books.reduce((prev, cur) => {
+    return [...prev, ...cur.genres];
+  }, []);
+
+  // filter out duplicates and add "all genres"
+  const filteredGenres = genresArr
+    .filter((genre, index) => genresArr.indexOf(genre) === index)
+    .concat('all genres');
+
+  const renderGenres = () => {
+    return filteredGenres.map((genre, i) => (
+      <button
+        key={i}
+        onClick={({ target }) => genreButtonHandler(target.innerText)}
+      >
+        {genre}
+      </button>
+    ));
+  };
+
+  const genreButtonHandler = (genre) => {
+    if (genre === 'all genres') {
+      setSelectedGenre(null);
+    }
+    setSelectedGenre(genre);
+  };
+
+  const renderBooks = () => {
+    return books
+      .filter((book) => {
+        return selectedGenre === 'all genres'
+          ? true
+          : book.genres.includes(selectedGenre);
+      })
+      .map((a) => (
+        <tr key={a.title}>
+          <td>{a.title}</td>
+          <td>{a.author.name}</td>
+          <td>{a.published}</td>
+        </tr>
+      ));
+  };
 
   return (
     <div>
@@ -24,15 +68,10 @@ const Books = (props) => {
             <th>Author</th>
             <th>Published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {renderBooks()}
         </tbody>
       </table>
+      {renderGenres()}
     </div>
   );
 };
