3c82eae23604f82399f1b4e207281ecfd00f4417
Luka <luka.dimnik88@gmail.com>
2022-06-14 15:36:00 +0300

    Part-9 Exercise 9.4

---
+++ part9/starting-exercises/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  console.log('hopla');
+
+  res.send('Hello Full Stack!');
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log('server running on port ' + PORT);
+});
